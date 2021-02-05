96de88ff572a1aaba77c7ac377d9039b6e861c8e
Ke Xu <kxu9@uci.edu>
2021-02-04 22:42:17 -0800

    Login page 3

---
+++ app/src/main/java/com/example/cs125_lifetuner/Login.java
@@ -0,0 +1,24 @@
+package com.example.cs125_lifetuner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //背景代码 每次建立新的activity都可以把这一段复制到onCreate方法中
+        LinearLayout background_Layout = (LinearLayout) findViewById(R.id.main_container);
+        AnimationDrawable animationDrawable = (AnimationDrawable) background_Layout.getBackground();
+        animationDrawable.setEnterFadeDuration(4000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+        //
+    }
+}
