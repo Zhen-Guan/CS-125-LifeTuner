33a449b4fb855960c7d7a0a3bf015387187a1579
Ke Xu <kxu9@uci.edu>
2021-03-09 00:39:42 -0800

    jsonparse2

--- app/src/main/java/com/example/cs125_lifetuner/Result_Exercise.java
+++ app/src/main/java/com/example/cs125_lifetuner/Result_Exercise.java
@@ -1,5 +1,6 @@
 package com.example.cs125_lifetuner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -8,30 +9,30 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.AnimationDrawable;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -42,7 +43,7 @@ public class Result_Exercise extends AppCompatActivity {
     int result_calories;
 
     // mapquest
-    private ListView exerciseNameList = findViewById(R.id.Exercise_Location);
+    private ListView exerciseNameList;
     FusedLocationProviderClient client;
 
     @Override
@@ -98,16 +99,30 @@ public class Result_Exercise extends AppCompatActivity {
             }
         });
         //更改完毕
+        exerciseNameList = findViewById(R.id.Exercise_Location);
         // getcurrent location
 //        client = LocationServices.getFusedLocationProviderClient(this);
 //        Location location = getCurrentLocation();
-//        String lat = Double.toString(location.getLatitude());
-//        String lng = Double.toString(location.getLongitude());
-//        Toast.makeText(this, "latitude:"+lat+"; longtitude:" + lng, Toast.LENGTH_SHORT).show();
+//        if (location != null) {
+//            String lat = Double.toString(location.getLatitude());
+//            String lng = Double.toString(location.getLongitude());
+//            Toast.makeText(this, "latitude:"+lat+"; longtitude:" + lng, Toast.LENGTH_SHORT).show();
+//
+//        }
+//        else {
+//            Toast.makeText(this, "still no latlng", Toast.LENGTH_SHORT).show();
+//        }
 
 
         // mapquest
+//        Location location = null;
 //        jsonParse(location);
+//        try {
+//            jsonParse2();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        new RetrieveFeedTask().execute();
 
         //背景代码 每次建立新的activity都可以把这一段复制到onCreate方法中
         LinearLayout background_Layout = (LinearLayout) findViewById(R.id.main_container);
@@ -141,42 +156,78 @@ public class Result_Exercise extends AppCompatActivity {
     }
 
     // mapquest
-    private void jsonParse(Location location) {
-        String lat = Double.toString(location.getLatitude());
-        String lng = Double.toString(location.getLongitude());
-        String url = "https://www.mapquestapi.com/search/v2/radius?origin=" + lat + ",+" + lng + "&radius=1&maxMatches=3&ambiguities=ignore&hostedData=mqap.ntpois|group_sic_code=?|799101&outFormat=json&key= OqC1DY34U3qYSVrCzrMVqw0AlcIcJ3AX ";
+//    private void jsonParse(Location location) {
+//        String url = "";
+//        if (location != null) {
+//            String lat = Double.toString(location.getLatitude());
+//            String lng = Double.toString(location.getLongitude());
+//            url = "https://www.mapquestapi.com/search/v2/radius?origin=" + lat + ",+" + lng + "&radius=1&maxMatches=3&ambiguities=ignore&hostedData=mqap.ntpois|group_sic_code=?|799101&outFormat=json&key= OqC1DY34U3qYSVrCzrMVqw0AlcIcJ3AX";
+//        } else {
+//            url = "https://www.mapquestapi.com/search/v2/radius?origin=33.646875,+-117.840508&radius=1&maxMatches=3&ambiguities=ignore&hostedData=mqap.ntpois|group_sic_code=?|799101&outFormat=json&key= OqC1DY34U3qYSVrCzrMVqw0AlcIcJ3AX";
+////            Toast.makeText(Result_Exercise.this, "here3", Toast.LENGTH_LONG).show();
+//        }
+//        ArrayList<String> resultList = new ArrayList<>();
+//        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+//                new Response.Listener<JSONObject>() {
+//                    @Override
+//                    public void onResponse(JSONObject response) {
+//                        Toast.makeText(Result_Exercise.this, "response", Toast.LENGTH_LONG).show();
+//                        try {
+//                            JSONArray jsonArray = response.getJSONArray("searchResults");
+//                            for (int i = 0; i < jsonArray.length(); i++) {
+//                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+//                                String resultName = jsonObject.getString("name");
+//                                resultList.add(resultName);
+//                            }
+//                        } catch (JSONException e) {
+//                            e.printStackTrace();
+//                        }
+//
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                error.printStackTrace();
+//            }
+//        });
+//
+////        if (resultList.isEmpty()) {
+////            Toast.makeText(Result_Exercise.this, "here4", Toast.LENGTH_LONG).show();
+////        }
+//        ArrayAdapter arrayAdapter = new ArrayAdapter<>(Result_Exercise.this, android.R.layout.simple_list_item_1, resultList);
+//        exerciseNameList.setAdapter(arrayAdapter);
+//    }
 
-//        String url = "https://www.mapquestapi.com/search/v2/radius?origin=33.646875,+-117.840508&radius=1&maxMatches=3&ambiguities=ignore&hostedData=mqap.ntpois|group_sic_code=?|799101&outFormat=json&key= OqC1DY34U3qYSVrCzrMVqw0AlcIcJ3AX ";
+    private void jsonParse2() throws IOException {
+        Toast.makeText(Result_Exercise.this, "HERE", Toast.LENGTH_LONG);
+
         ArrayList<String> resultList = new ArrayList<>();
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            JSONArray jsonArray = response.getJSONArray("searchResults");
-                            for (int i = 0; i < jsonArray.length(); i++) {
-                                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                                String resultName = jsonObject.getString("name");
-                                resultList.add(resultName);
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+        String sURL = "https://www.mapquestapi.com/search/v2/radius?origin=33.646875,+-117.840508&radius=1&maxMatches=3&ambiguities=ignore&hostedData=mqap.ntpois|group_sic_code=?|799101&outFormat=json&key= OqC1DY34U3qYSVrCzrMVqw0AlcIcJ3AX";
+        // Connect to the URL using java's native library
+        URL url = new URL(sURL);
+        URLConnection request = url.openConnection();
+        request.connect();
 
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
-            }
-        });
+        // Convert to a JSON object to print data
+        JsonParser jp = new JsonParser(); //from gson
+        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
+        JsonObject rootobj = root.getAsJsonObject(); //May be an array, may be an object.
+        JsonArray jsonArray = rootobj.getAsJsonArray("searchResults");
+        for (int i = 0; i < jsonArray.size(); i++) {
+            JsonObject jsonObject = jsonArray.get(i).getAsJsonObject();
+            String resultName = jsonObject.get("name").getAsString();
+            resultList.add(resultName);
+        }
+        if (!resultList.isEmpty()) {
+            Toast.makeText(Result_Exercise.this, "parsed", Toast.LENGTH_LONG);
+        } else {
+            Toast.makeText(Result_Exercise.this, "failed", Toast.LENGTH_LONG);
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter<>(Result_Exercise.this, android.R.layout.simple_list_item_1, resultList);
-        exerciseNameList.setAdapter(arrayAdapter);
+        }
     }
 
     private Location getCurrentLocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -184,8 +235,17 @@ public class Result_Exercise extends AppCompatActivity {
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
-            return null;
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            Toast.makeText(this, "here1", Toast.LENGTH_LONG).show();
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "here2", Toast.LENGTH_LONG).show();
+                return null;
+            }
         }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.INTERNET}, 1);
+        }
+
         Task<Location> task = client.getLastLocation();
         final Location[] resultLocation = new Location[1];
         task.addOnSuccessListener(new OnSuccessListener<Location>() {
@@ -194,10 +254,45 @@ public class Result_Exercise extends AppCompatActivity {
                 if(location!=null) {
                     resultLocation[0] = location;
                 }
+                else {
+                    Toast.makeText(Result_Exercise.this, "here3", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
         return resultLocation[0];
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "PERMISSION GRANTED", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "PERMISSION DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private class RetrieveFeedTask extends AsyncTask<Void, Void, Void> {
+
+        private Exception exception;
+
+        protected Void doInBackground(Void... voids) {
+            try {
+                jsonParse2();
+            } catch (Exception e) {
+                this.exception = e;
+
+                return null;
+            }
+            return null;
+        }
+
+        protected void onPostExecute(Void voids) {
+            // TODO: check this.exception
+            // TODO: do something with the feed
+        }
     }
 }
 
